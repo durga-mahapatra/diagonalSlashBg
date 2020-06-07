463fcb4b0589bd6e5bfb20228c49a423103db100
durgamadhab <durgamadhab@kuliza.com>
2020-06-07 23:37:11 +0530

    initial commit

---
+++ app/src/main/java/com/durga/halfslashlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by durga-mahapatra
+ */
+
+package com.durga.halfslashlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
